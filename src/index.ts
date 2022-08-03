f7c2dfdbb3ef16f5713f75ef287acb1833871f2b
Samuel Lippert <lippertz@gmail.com>
2022-08-03 18:29:03 -0500

    Add Toucan boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import type { Handler } from 'hono'
+import Toucan from 'toucan-js'
 
 export const hello = (): Handler => {
   return async (c, next) => {
-    await next()
+    const sentry = new Toucan({
+      dsn: c.env.SENTRY_DSN,
+      request: c.req,
+      allowedHeaders: ['user-agent'],
+      allowedSearchParams: /(.*)/,
+    })
+
+    try {
+      await next()
+    } catch (error) {
+      sentry.captureException(error)
+    }
   }
 }
