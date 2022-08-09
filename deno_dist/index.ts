992df19dd15c5a31a0951ef46a820b2e9a73ad11
Samuel Lippert <lippertz@gmail.com>
2022-08-09 11:08:36 -0500

    Update README + deno index

--- deno_dist/index.ts
+++ deno_dist/index.ts
@@ -1,8 +1,19 @@
 import type { Handler } from 'https://raw.githubusercontent.com/honojs/hono/v2.0.6/deno_dist/mod.ts'
+import Toucan from 'toucan-js'
 
-export const hello = (message: string = 'Hello'): Handler => {
+export const hello = (): Handler => {
   return async (c, next) => {
-    await next()
-    c.res.headers.append('X-Message', message)
+    const sentry = new Toucan({
+      dsn: c.env.SENTRY_DSN,
+      request: c.req,
+      allowedHeaders: ['user-agent'],
+      allowedSearchParams: /(.*)/,
+    })
+
+    try {
+      await next()
+    } catch (error) {
+      sentry.captureException(error)
+    }
   }
 }
