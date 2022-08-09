e667634a3d92ed74ea18b0c6f9ca371dab4468c7
Samuel Lippert <lippertz@gmail.com>
2022-08-09 14:09:45 -0500

    Import placeholder

--- deno_dist/index.ts
+++ deno_dist/index.ts
@@ -1,16 +1,17 @@
 import type { Handler } from 'https://raw.githubusercontent.com/honojs/hono/v2.0.6/deno_dist/mod.ts'
+// import Toucan from '../node_modules/toucan-js/dist/index.d.ts'
 
 export const sentry = (): Handler => {
   return async (c, next) => {
-    //   const sentry = new Toucan({
-    //     dsn: c.env.SENTRY_DSN,
-    //     request: c.req,
-    //     allowedHeaders: ['user-agent'],
-    //     allowedSearchParams: /(.*)/,
-    //   })
+    // const sentry = new Toucan({
+    //   dsn: c.env.SENTRY_DSN,
+    //   request: c.req,
+    //   allowedHeaders: ['user-agent'],
+    //   allowedSearchParams: /(.*)/,
+    // })
 
     // try {
-    await next()
+      await next()
     // } catch (error) {
     //   sentry.captureException(error)
     // }
