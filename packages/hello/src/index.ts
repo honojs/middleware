cfd961df6bbd2fb29617cc8d872f95b0460cbe50
Yusuke Wada <yusuke@kamawada.com>
2023-01-13 07:57:14 +0900

    fix(hello): use `MiddlewareHandler`

--- packages/hello/src/index.ts
+++ packages/hello/src/index.ts
@@ -1,6 +1,6 @@
-import type { Handler } from 'hono'
+import type { MiddlewareHandler } from 'hono'
 
-export const hello = (message: string = 'Hello!'): Handler => {
+export const hello = (message: string = 'Hello!'): MiddlewareHandler => {
   return async (c, next) => {
     await next()
     c.res.headers.append('X-Message', message)
