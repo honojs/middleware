e0f68909ea2790d8857d3b79888ebe527b79085f
Yusuke Wada <yusuke@kamawada.com>
2023-02-04 20:20:42 +0900

    setup Firebase Auth middleware

--- packages/firebase-auth/jest.config.js
+++ packages/firebase-auth/jest.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  testMatch: [
-    "**/test/**/*.+(ts|tsx|js)",
-    "**/src/**/(*.)+(spec|test).+(ts|tsx|js)",
-  ],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  testEnvironment: "miniflare",
-  testEnvironmentOptions: {
-    kvNamespaces: ["PUBLIC_JWK_CACHE_KV"],
-  },
-};
+module.exports = require('../../jest.config.js')
--- packages/firebase-auth/src/index.ts
+++ packages/firebase-auth/src/index.ts
@@ -1,89 +1,85 @@
-import { Context, Handler } from "hono";
-import {
-  EmulatorEnv,
-  Auth,
-  WorkersKVStoreSingle,
-  KeyStorer,
-  FirebaseIdToken,
-} from "firebase-auth-cloudflare-workers";
+import type { EmulatorEnv, KeyStorer, FirebaseIdToken } from 'firebase-auth-cloudflare-workers'
+import { Auth, WorkersKVStoreSingle } from 'firebase-auth-cloudflare-workers'
+import type { Context, MiddlewareHandler } from 'hono'
 
 export interface VerifyFirebaseAuthEnv extends EmulatorEnv {
-  PUBLIC_JWK_CACHE_KEY?: string | undefined;
-  PUBLIC_JWK_CACHE_KV?: KVNamespace | undefined;
+  PUBLIC_JWK_CACHE_KEY?: string | undefined
+  PUBLIC_JWK_CACHE_KV?: KVNamespace | undefined
 }
 
 export interface VerifyFirebaseAuthConfig {
-  projectId: string;
-  authorizationHeaderKey?: string;
-  keyStore?: KeyStorer;
-  keyStoreInitializer?: (c: Context) => KeyStorer;
-  disableErrorLog?: boolean;
-  firebaseEmulatorHost?: string;
+  projectId: string
+  authorizationHeaderKey?: string
+  keyStore?: KeyStorer
+  keyStoreInitializer?: (c: Context) => KeyStorer
+  disableErrorLog?: boolean
+  firebaseEmulatorHost?: string
 }
 
-const defaultKVStoreJWKCacheKey = "verify-firebase-auth-cached-public-key";
+const defaultKVStoreJWKCacheKey = 'verify-firebase-auth-cached-public-key'
 const defaultKeyStoreInitializer = (c: Context): KeyStorer => {
   return WorkersKVStoreSingle.getOrInitialize(
     c.env.PUBLIC_JWK_CACHE_KEY ?? defaultKVStoreJWKCacheKey,
     c.env.PUBLIC_JWK_CACHE_KV
-  );
-};
+  )
+}
 
-export const verifyFirebaseAuth = (
-  userConfig: VerifyFirebaseAuthConfig
-): Handler => {
+type Env = {
+  Bindings: {
+    FIREBASE_AUTH_EMULATOR_HOST: string
+  }
+}
+
+export const verifyFirebaseAuth = (userConfig: VerifyFirebaseAuthConfig): MiddlewareHandler => {
   const config = {
     projectId: userConfig.projectId,
-    AuthorizationHeaderKey:
-      userConfig.authorizationHeaderKey ?? "Authorization",
+    AuthorizationHeaderKey: userConfig.authorizationHeaderKey ?? 'Authorization',
     KeyStore: userConfig.keyStore,
-    keyStoreInitializer:
-      userConfig.keyStoreInitializer ?? defaultKeyStoreInitializer,
+    keyStoreInitializer: userConfig.keyStoreInitializer ?? defaultKeyStoreInitializer,
     disableErrorLog: userConfig.disableErrorLog,
     firebaseEmulatorHost: userConfig.firebaseEmulatorHost,
-  };
+  }
 
   return async (c, next) => {
-    const authorization = c.req.headers.get(config.AuthorizationHeaderKey);
+    const authorization = c.req.headers.get(config.AuthorizationHeaderKey)
     if (authorization === null) {
       return new Response(null, {
         status: 400,
-      });
+      })
     }
-    const jwt = authorization.replace(/Bearer\s+/i, "");
+    const jwt = authorization.replace(/Bearer\s+/i, '')
     const auth = Auth.getOrInitialize(
       config.projectId,
       config.KeyStore ?? config.keyStoreInitializer(c)
-    );
+    )
 
     try {
       const idToken = await auth.verifyIdToken(jwt, {
         FIREBASE_AUTH_EMULATOR_HOST:
           config.firebaseEmulatorHost ?? c.env.FIREBASE_AUTH_EMULATOR_HOST,
-      });
-      setFirebaseToken(c, idToken);
+      })
+      setFirebaseToken(c, idToken)
     } catch (err) {
       if (!userConfig.disableErrorLog) {
         console.error({
-          message: "failed to verify the requested firebase token",
+          message: 'failed to verify the requested firebase token',
           err,
-        });
+        })
       }
       return new Response(null, {
         status: 401,
-      });
+      })
     }
-    await next();
-  };
-};
+    await next()
+  }
+}
 
-const idTokenContextKey = "firebase-auth-cloudflare-id-token-key";
+const idTokenContextKey = 'firebase-auth-cloudflare-id-token-key'
 
-const setFirebaseToken = (c: Context, idToken: FirebaseIdToken) =>
-  c.set(idTokenContextKey, idToken);
+const setFirebaseToken = (c: Context, idToken: FirebaseIdToken) => c.set(idTokenContextKey, idToken)
 
 export const getFirebaseToken = (c: Context): FirebaseIdToken | null => {
-  const idToken = c.get(idTokenContextKey);
-  if (!idToken) return null;
-  return idToken;
-};
+  const idToken = c.get(idTokenContextKey)
+  if (!idToken) return null
+  return idToken
+}
