95849c8e066cf019e54096d0d6c7942c393579ba
ThinhVu <vutrongthinhk7@gmail.com>
2023-04-30 12:40:04 +0700

    clean code

--- packages/mongodb-atlas/src/index.ts
+++ packages/mongodb-atlas/src/index.ts
@@ -1,17 +1,15 @@
 import type { Context, Next } from 'hono'
 import * as Realm from 'realm-web'
 
-interface Bindings {
-   // MongoDB Realm Application ID
-   REALM_APPID: string;
-   REALM_API_KEY: string;
-}
+import RealmServices = globalThis.Realm.Services
+import MongoDB = RealmServices.MongoDB
 
-type Document = globalThis.Realm.Services.MongoDB.Document;
+type MongoDBDatabase = RealmServices.MongoDBDatabase
+type Document = MongoDB.Document
 
 let App: Realm.App
 let user
-let client: globalThis.Realm.Services.MongoDB
+let client: MongoDB
 
 const KEYS = {
    APP: 'mongodb-atlas-app',
@@ -19,8 +17,8 @@ const KEYS = {
    CLIENT: 'mongodb-atlas-client',
 }
 
-const dbCache: Record<string, globalThis.Realm.Services.MongoDBDatabase> = {}
-const collectionCache: Record<string, globalThis.Realm.Services.MongoDB.MongoDBCollection<Document>> = {}
+const dbCache: Record<string, MongoDBDatabase> = {}
+const collectionCache: Record<string, MongoDB.MongoDBCollection<Document>> = {}
 
 export interface IMongoDBAtlasOptions {
    realmAppId: string;
@@ -57,18 +55,17 @@ export function getUser(c: Context) {
    return c.get(KEYS.USER)
 }
 
-export function getClient(c: Context): globalThis.Realm.Services.MongoDB {
+export function getClient(c: Context): MongoDB {
    return c.get(KEYS.CLIENT)
 }
 
-export function getDb(c: Context, name: string): globalThis.Realm.Services.MongoDBDatabase {
+export function getDb(c: Context, name: string): MongoDBDatabase {
    if (!dbCache[name])
       dbCache[name] = getClient(c).db(name)
    return dbCache[name]
 }
 
-export function getCollection<T extends Document>(c: Context, dbName: string, collectionName: string)
-   : globalThis.Realm.Services.MongoDB.MongoDBCollection<T> {
+export function getCollection<T extends Document>(c: Context, dbName: string, collectionName: string): MongoDB.MongoDBCollection<T> {
    if (!collectionCache[collectionName])
       collectionCache[collectionName] = getDb(c, dbName).collection<T>(collectionName)
    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
