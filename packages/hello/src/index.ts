4c4d4eb4221c1d38683fa5021442d2a1fdfe0da6
Yusuke Wada <yusuke@kamawada.com>
2023-11-03 05:49:40 +0900

    feat(hello): infer type correctly using `satisfies`

--- packages/hello/src/index.ts
+++ packages/hello/src/index.ts
@@ -1,8 +1,8 @@
 import type { MiddlewareHandler } from 'hono'
 
-export const hello = (message: string = 'Hello!'): MiddlewareHandler => {
-  return async (c, next) => {
+export const hello = (message: string = 'Hello!') => {
+  return (async (c, next) => {
     await next()
     c.res.headers.append('X-Message', message)
-  }
+  }) satisfies MiddlewareHandler
 }
