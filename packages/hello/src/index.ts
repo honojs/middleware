a3da3dac910e24075466932ac39bb4e529dfb483
Yusuke Wada <yusuke@kamawada.com>
2023-11-05 16:45:07 +0900

    chore(hello): tweak (#235)
    * chore(hello): tweak
    
    * changeset

--- packages/hello/src/index.ts
+++ packages/hello/src/index.ts
@@ -1,8 +1,8 @@
-import type { MiddlewareHandler } from 'hono'
+import { createMiddleware } from 'hono/factory'
 
 export const hello = (message: string = 'Hello!') => {
-  return (async (c, next) => {
+  return createMiddleware(async (c, next) => {
     await next()
     c.res.headers.append('X-Message', message)
-  }) satisfies MiddlewareHandler
+  })
 }
