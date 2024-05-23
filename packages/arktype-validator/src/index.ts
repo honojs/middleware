c05e5456cb10b6a61df8a203d11f5588b696e824
David Blass <david@arktype.io>
2024-05-23 16:59:20 -0400

    feat(arktype-validator): migrate to 2.0 (#542)
    * feat(arktype-validator): migrate to 2.0
    
    * add changeset

--- packages/arktype-validator/src/index.ts
+++ packages/arktype-validator/src/index.ts
@@ -1,17 +1,16 @@
-import type { Type, Problems } from 'arktype'
+import { type, type Type, type ArkErrors } from 'arktype'
 import type { Context, MiddlewareHandler, Env, ValidationTargets, TypedResponse } from 'hono'
 import { validator } from 'hono/validator'
 
 export type Hook<T, E extends Env, P extends string, O = {}> = (
-  result: { success: false; data: unknown; problems: Problems } | { success: true; data: T },
+  result: { success: false; data: unknown; errors: ArkErrors } | { success: true; data: T },
   c: Context<E, P>
 ) => Response | Promise<Response> | void | Promise<Response | void> | TypedResponse<O>
 
 type HasUndefined<T> = undefined extends T ? true : false
 
 export const arktypeValidator = <
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  T extends Type<any>,
+  T extends Type,
   Target extends keyof ValidationTargets,
   E extends Env,
   P extends string,
@@ -30,11 +29,13 @@ export const arktypeValidator = <
   hook?: Hook<T['infer'], E, P>
 ): MiddlewareHandler<E, P, V> =>
   validator(target, (value, c) => {
-    const { data, problems } = schema(value)
+    const out = schema(value)
+
+    const hasErrors = out instanceof type.errors
 
     if (hook) {
       const hookResult = hook(
-        problems ? { success: false, data: value, problems } : { success: true, data },
+        hasErrors ? { success: false, data: value, errors: out } : { success: true, data: out },
         c
       )
       if (hookResult) {
@@ -47,15 +48,15 @@ export const arktypeValidator = <
       }
     }
 
-    if (problems) {
+    if (hasErrors) {
       return c.json(
         {
           success: false,
-          problems: problems.map((problem) => ({ ...problem, message: problem.toString() })),
+          errors: out,
         },
         400
       )
     }
 
-    return data
+    return out
   })
