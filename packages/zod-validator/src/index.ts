f96a00ac912ce810d9b8b2ef6e505deda4cc3855
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-05-29 02:02:40 +0000

    fix(zod-validator): validator support async hook

--- packages/zod-validator/src/index.ts
+++ packages/zod-validator/src/index.ts
@@ -5,7 +5,7 @@ import type { z, ZodSchema, ZodError } from 'zod'
 export type Hook<T, E extends Env, P extends string, O = {}> = (
   result: { success: true; data: T } | { success: false; error: ZodError; data: T },
   c: Context<E, P>
-) => Response | Promise<Response> | void | Promise<Response | void> | TypedResponse<O>
+) => Response | Promise<Response> | void | Promise<void> | TypedResponse<O> | Promise<TypedResponse<O>>
 
 type HasUndefined<T> = undefined extends T ? true : false
 
@@ -45,7 +45,7 @@ export const zValidator = <
     const result = await schema.safeParseAsync(value)
 
     if (hook) {
-      const hookResult = hook({ data: value, ...result }, c)
+      const hookResult = await hook({ data: value, ...result }, c)
       if (hookResult) {
         if (hookResult instanceof Response || hookResult instanceof Promise) {
           return hookResult
