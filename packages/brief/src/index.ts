5031d7a41e1c430e00dc84d6551ee1c7d21d805f
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-22 12:33:20 +0200

    feat(brief-middleware): add brief middleware

---
+++ packages/brief/src/index.ts
@@ -0,0 +1,30 @@
+import { createMiddleware } from 'hono/factory'
+import { HonoBase } from 'hono/hono-base'
+
+const INFO_COLOR = '\x1b[36m'
+const RESET_COLOR = '\x1b[0m'
+
+export const brief = (app: HonoBase) => {
+  return createMiddleware(async (_, next) => {
+    const uniqueRoutes = app.routes.filter((value, index) => {
+      const _value = JSON.stringify(value)
+
+      return (
+        index ===
+        app.routes.findIndex((obj) => {
+          return JSON.stringify(obj) === _value
+        })
+      )
+    })
+
+    const mappedRoutes = uniqueRoutes
+      .filter(({ method }) => method != 'ALL')
+      .map(({ path, method }) => ({ path, method }))
+
+    console.info(`${INFO_COLOR}[INFO] APP ROUTES:${RESET_COLOR}`)
+    console.table(mappedRoutes)
+    console.log()
+
+    await next()
+  })
+}
