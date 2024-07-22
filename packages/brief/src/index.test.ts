432ed7389ca6c31080a1024cce0ebe9e65d31f66
pietrodev07 <pietro.dev.07@gmail.com>
2024-07-22 12:39:59 +0200

    test(brief-middleware): add test for custom title

--- packages/brief/src/index.test.ts
+++ packages/brief/src/index.test.ts
@@ -12,16 +12,6 @@ beforeEach(async () => {
   console.table = consoleTableMock
   consoleInfoMock.mockClear()
   consoleTableMock.mockClear()
-
-  const app = new Hono()
-  app.use(brief(app))
-
-  app.get('/route1', (c) => c.text('route1'))
-  app.get('/route2', (c) => c.text('route2'))
-  app.get('/route1', (c) => c.text('route')) // duplicated route
-
-  const response = await app.request('http://localhost/route1')
-  expect(response).toBeDefined()
 })
 
 afterEach(() => {
@@ -30,11 +20,31 @@ afterEach(() => {
 })
 
 describe('Brief middleware', () => {
-  it('Should log title', async () => {
+  it('Should log default title', async () => {
+    const app = new Hono()
+    app.use('*', brief(app))
+
+    await app.request('http://localhost')
     expect(consoleInfoMock).toHaveBeenCalledWith(expect.stringContaining('[INFO] APP ROUTES:'))
   })
 
+  it('Should log custom title', async () => {
+    const app = new Hono()
+    app.use('*', brief(app, '[INFO] CUSTOM TITLE'))
+
+    await app.request('http://localhost')
+    expect(consoleInfoMock).toHaveBeenCalledWith(expect.stringContaining('[INFO] CUSTOM TITLE:'))
+  })
+
   it('Should log table without duplicating and ALL method', async () => {
+    const app = new Hono()
+    app.use('*', brief(app))
+
+    app.get('/route1', (c) => c.text('route1'))
+    app.get('/route2', (c) => c.text('route2'))
+    app.get('/route1', (c) => c.text('route')) // duplicated route
+
+    await app.request('http://localhost')
     expect(consoleTableMock).toHaveBeenCalledWith([
       { path: '/route1', method: 'GET' },
       { path: '/route2', method: 'GET' },
