5ed20846e8324995394d992ddb206acd24cc8461
uttk <46495635+uttk@users.noreply.github.com>
2024-07-27 13:09:38 +0000

    fix: Fixed to return 400 response when the request body is not FormData

--- packages/conform-validator/src/utils.ts
+++ packages/conform-validator/src/utils.ts
@@ -1,9 +1,13 @@
 import type { Context } from 'hono'
 import { bufferToFormData } from 'hono/utils/buffer'
 
+// ref: https://github.com/honojs/hono/blob/a63bcfd6fba66297d8234c21aed8a42ac00711fe/src/validator/validator.ts#L27-L28
+const multipartRegex = /^multipart\/form-data(; boundary=[A-Za-z0-9'()+_,\-./:=?]+)?$/
+const urlencodedRegex = /^application\/x-www-form-urlencoded$/
+
 export const getFormDataFromContext = async (ctx: Context): Promise<FormData> => {
   const contentType = ctx.req.header('Content-Type')
-  if (!contentType) {
+  if (!contentType || !(multipartRegex.test(contentType) || urlencodedRegex.test(contentType))) {
     return new FormData()
   }
 
