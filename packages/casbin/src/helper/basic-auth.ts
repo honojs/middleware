e0247dfea6dc7d78eb0e1e94bd34df606b79a638
sugar-cat7 <study.king7777@gmail.com>
2024-09-03 18:12:55 +0900

    fixed: hono version

--- packages/casbin/src/helper/basic-auth.ts
+++ packages/casbin/src/helper/basic-auth.ts
@@ -1,34 +1,9 @@
 import type { Enforcer } from 'casbin'
-import type { Context, HonoRequest } from 'hono'
-import { decodeBase64 } from 'hono/utils/encode'
-
-// NOTE: Hono's basic-auth middleware implementation:
-// https://github.com/honojs/hono/blob/8ba02273e829318d7f8797267f52229e531b8bd5/src/middleware/basic-auth/index.ts#L16-L33
-const CREDENTIALS_REGEXP = /^ *(?:[Bb][Aa][Ss][Ii][Cc]) +([A-Za-z0-9._~+/-]+=*) *$/
-const USER_PASS_REGEXP = /^([^:]*):(.*)$/
-const utf8Decoder = new TextDecoder()
-
-const auth = (req: HonoRequest) => {
-  const match = CREDENTIALS_REGEXP.exec(req.header('Authorization') || '')
-  if (!match) {
-    return undefined
-  }
-
-  let userPass = undefined
-  // If an invalid string is passed to atob(), it throws a `DOMException`.
-  try {
-    userPass = USER_PASS_REGEXP.exec(utf8Decoder.decode(decodeBase64(match[1])))
-  } catch {} // Do nothing
-
-  if (!userPass) {
-    return undefined
-  }
-
-  return { username: userPass[1], password: userPass[2] }
-}
+import type { Context } from 'hono'
+import { auth } from 'hono/utils/basic-auth'
 
 const getUserName = (c: Context): string => {
-  const requestUser = auth(c.req)
+  const requestUser = auth(c.req.raw)
   if (!requestUser) {
     return ''
   }
