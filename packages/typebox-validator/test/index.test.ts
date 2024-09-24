39cb252df9a0829b8397b9edbcc1de27e49230e1
Lindelwe Michael Ncube <lindelwencube.ln@gmail.com>
2024-09-24 17:04:59 +0700

    fix(tbValidator): Fix tbValidator not calling hook when there's a fail case (#742)
    * Fix tbValidator not calling hook when there's a fail case
    
    * add tests
    
    * Add changeset

--- packages/typebox-validator/test/index.test.ts
+++ packages/typebox-validator/test/index.test.ts
@@ -1,7 +1,8 @@
-import { Type as T } from '@sinclair/typebox'
+import { Type as T, TypeBoxError } from '@sinclair/typebox';
 import { Hono } from 'hono'
 import type { Equal, Expect } from 'hono/utils/types'
 import { tbValidator } from '../src'
+import { ValueError } from '@sinclair/typebox/value';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type ExtractSchema<T> = T extends Hono<infer _, infer S> ? S : never
@@ -106,6 +107,18 @@ describe('With Hook', () => {
         message: `${data.id} is ${data.title}`,
       })
     }
+  ).post(
+    '/errorTest',
+    tbValidator('json', schema, (result, c) => {
+      return c.json(result, 400)
+    }),
+    (c) => {
+      const data = c.req.valid('json')
+      return c.json({
+        success: true,
+        message: `${data.id} is ${data.title}`,
+      })
+    }
   )
 
   it('Should return 200 response', async () => {
@@ -139,5 +152,40 @@ describe('With Hook', () => {
     const res = await app.request(req)
     expect(res).not.toBeNull()
     expect(res.status).toBe(400)
+  })
+
+  it('Should return 400 response and error array', async () => {
+    const req = new Request('http://localhost/errorTest', {
+      body: JSON.stringify({
+        id: 123,
+      }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    const res = await app.request(req)
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(400)
+
+    const {errors, success} = (await res.json()) as { success: boolean; errors: any[] }
+    expect(success).toBe(false)
+    expect(Array.isArray(errors)).toBe(true)
+    expect(errors.map((e: ValueError) => ({
+      'type': e?.schema?.type,
+        path: e?.path,
+        message: e?.message
+    }))).toEqual([
+      {
+        "type":  "string",
+        "path": "/title",
+        "message": "Required property"
+      },
+      {
+        "type":  "string",
+        "path": "/title",
+        "message": "Expected string"
+      }
+    ])
   })
 })
