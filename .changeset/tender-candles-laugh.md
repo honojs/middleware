5fd80263f28180627430c36f97090b8541bcdcfb
Ogabek <70449019+OgabekYuldoshev@users.noreply.github.com>
2024-11-05 07:00:20 +0500

    feat: introduce Swagger Editor middleware (#800)
    Closes https://github.com/honojs/hono/issues/1415
    
    * chore(swagger-editor): :hammer: init
    
    * ci: :ferris_wheel: swagger editor workflow
    
    create workflow for swagger-editor package
    
    * docs: :memo: readme
    
    have writed documentation
    
    * test(test): :white_check_mark: create test for swagger-editor middleware
    
    * fix(swagger-editor): :bug: fixed cdn url in html content
    
    * chore: :hammer: v0.1.0
    
    release
    
    * format
    
    * fix the typos
    
    * remove unnecessary `.`
    
    * remove unnecessay `vite` and fix the `vitest.config.ts`
    
    * fix the relase workflow
    
    * update the changeset and `package.json`
    
    ---------
    
    Co-authored-by: Yusuke Wada <yusuke@kamawada.com>

---
+++ .changeset/tender-candles-laugh.md
@@ -0,0 +1,5 @@
+---
+'@hono/swagger-editor': major
+---
+
+Create swagger editor middleware for hono
---
+++ packages/swagger-editor/test/index.test.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono'
+import { swaggerEditor } from '../src'
+
+describe('Swagger Editor Middleware', () => {
+  let app: Hono
+
+  beforeEach(() => {
+    app = new Hono()
+  })
+
+  it('responds with status 200', async () => {
+    app.get('/swagger-editor', swaggerEditor())
+
+    const res = await app.request('/swagger-editor')
+    expect(res.status).toBe(200)
+  })
+
+  it('should contents shown', async () => {
+    app.get(
+      '/swagger-editor',
+      swaggerEditor({
+        url: 'https://petstore3.swagger.io/api/v3/openapi.json',
+      })
+    )
+
+    const res = await app.request('/swagger-editor')
+    const html = await res.text()
+
+    expect(html).toContain('https://petstore3.swagger.io/api/v3/openapi.json')
+    expect(html).toContain('https://cdn.jsdelivr.net/npm/swagger-editor-dist')
+  })
+})
