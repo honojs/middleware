af7be31bb3df1edb2aa127b3fef3bad3c4af6b6d
Daniel Pedersen <contact@danielpedersen.se>
2024-11-15 10:08:35 +0100

    add test

--- packages/zod-openapi/test/handler.test-d.ts
+++ packages/zod-openapi/test/handler.test-d.ts
@@ -1,3 +1,4 @@
+import type { MiddlewareHandler } from 'hono'
 import type { RouteHandler } from '../src'
 import { OpenAPIHono, createRoute, z } from '../src'
 
@@ -51,5 +52,41 @@ describe('supports async handler', () => {
     const hono = new OpenAPIHono()
     hono.openapi(route, handler)
     hono.openapi(route, asyncHandler)
+  })
+
+  test('RouteHandler infers env type from middleware', () => {
+    type CustomEnv = { Variables: { customKey: string } }
+
+    const customMiddleware: MiddlewareHandler<CustomEnv> = (c, next) => {
+      c.set('customKey', 'customValue')
+      return next()
+    }
+
+    const routeWithMiddleware = createRoute({
+      method: 'get',
+      path: '/users',
+      middleware: [customMiddleware] as const,
+      responses: {
+        200: {
+          content: {
+            'application/json': {
+              schema: z.object({
+                id: z.string(),
+              }),
+            },
+          },
+          description: 'Retrieve the user',
+        },
+      },
+    })
+
+    const handler: RouteHandler<typeof routeWithMiddleware> = (c) => {
+      return c.json({
+        id: c.get('customKey'),
+      })
+    }
+
+    const hono = new OpenAPIHono()
+    hono.openapi(routeWithMiddleware, handler)
   })
 })
