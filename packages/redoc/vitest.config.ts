53823fae851ccbad3df93dddc8636e9ff2ab0347
jonghyo <m.73816686@gmail.com>
2024-12-18 10:49:08 +0900

    chore(redoc):update devDependencies

--- packages/redoc/vitest.config.ts
+++ packages/redoc/vitest.config.ts
@@ -3,5 +3,20 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['cobertura', 'lcov', 'json', 'html', 'text', 'text-summary'],
+      reportsDirectory: './coverage',
+      reportOnFailure: true,
+      watermarks: {
+        lines: [50, 80],
+        functions: [50, 80],
+        branches: [50, 80],
+        statements: [50, 80],
+      },
+      all: true,
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/**/*.test.{ts,tsx}'],
+    },
   },
 })
