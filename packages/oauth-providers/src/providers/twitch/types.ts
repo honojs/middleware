23cd8575fc709bddd008cae816cf397f110cfef6
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-02-23 16:42:45 +0300

    feat(twitch): Add type definitions for Twitch OAuth scopes

---
+++ packages/oauth-providers/src/providers/twitch/types.ts
@@ -0,0 +1,36 @@
+export type Scopes =
+  // Analytics
+  | 'analytics:read:extensions'
+  | 'analytics:read:games'
+  
+  // Bits
+  | 'bits:read'
+  
+  // Channel
+  | 'channel:bot'
+  | 'channel:manage:ads'
+  | 'channel:read:ads'
+  | 'channel:manage:broadcast'
+  | 'channel:read:charity'
+  | 'channel:edit:commercial'
+  | 'channel:read:editors'
+  | 'channel:manage:extensions'
+  | 'channel:read:goals'
+  | 'channel:read:guest_star'
+  | 'channel:manage:guest_star'
+  | 'channel:read:hype_train'
+  | 'channel:manage:moderators'
+  | 'channel:read:polls'
+  | 'channel:manage:polls'
+  | 'channel:read:predictions'
+  | 'channel:manage:predictions'
+  | 'channel:manage:raids'
+  | 'channel:read:redemptions'
+  | 'channel:manage:redemptions'
+  | 'channel:manage:schedule'
+  | 'channel:read:stream_key'
+  | 'channel:read:subscriptions'
+  | 'channel:manage:videos'
+  | 'channel:read:vips'
+  | 'channel:manage:vips'
+  | 'channel:moderate'
