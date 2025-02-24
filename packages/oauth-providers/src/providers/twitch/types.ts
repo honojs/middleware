0ea422c461c64604fc7db26bbe1c4b472aefb111
Younis-Ahmed <23105954+jonaahmed@users.noreply.github.com>
2025-02-24 16:46:43 +0300

    feat(twitch): Add new type definitions for Twitch API responses

--- packages/oauth-providers/src/providers/twitch/types.ts
+++ packages/oauth-providers/src/providers/twitch/types.ts
@@ -35,42 +35,81 @@ export type Scopes =
   | 'channel:manage:vips'
   | 'channel:moderate'
 
-    // Clips
-    | 'clips:edit'
-  
-    // Moderation
-    | 'moderation:read'
-    | 'moderator:manage:announcements'
-    | 'moderator:manage:automod'
-    | 'moderator:read:automod_settings'
-    | 'moderator:manage:automod_settings'
-    | 'moderator:read:banned_users'
-    | 'moderator:manage:banned_users'
-    | 'moderator:read:blocked_terms'
-    | 'moderator:read:chat_messages'
-    | 'moderator:manage:blocked_terms'
-    | 'moderator:manage:chat_messages'
-    | 'moderator:read:chat_settings'
-    | 'moderator:manage:chat_settings'
-    | 'moderator:read:chatters'
-    | 'moderator:read:followers'
-    | 'moderator:read:guest_star'
-    | 'moderator:manage:guest_star'
-    | 'moderator:read:moderators'
-    | 'moderator:read:shield_mode'
-    | 'moderator:manage:shield_mode'
-    | 'moderator:read:shoutouts'
-    | 'moderator:manage:shoutouts'
-    | 'moderator:read:suspicious_users'
-    | 'moderator:read:unban_requests'
-    | 'moderator:manage:unban_requests'
-    | 'moderator:read:vips'
-    | 'moderator:read:warnings'
-    | 'moderator:manage:warnings'
-      
-    // IRC Chat Scopes
-    | 'chat:edit'
-    | 'chat:read'
+  // Clips
+  | 'clips:edit'
+
+  // Moderation
+  | 'moderation:read'
+  | 'moderator:manage:announcements'
+  | 'moderator:manage:automod'
+  | 'moderator:read:automod_settings'
+  | 'moderator:manage:automod_settings'
+  | 'moderator:read:banned_users'
+  | 'moderator:manage:banned_users'
+  | 'moderator:read:blocked_terms'
+  | 'moderator:read:chat_messages'
+  | 'moderator:manage:blocked_terms'
+  | 'moderator:manage:chat_messages'
+  | 'moderator:read:chat_settings'
+  | 'moderator:manage:chat_settings'
+  | 'moderator:read:chatters'
+  | 'moderator:read:followers'
+  | 'moderator:read:guest_star'
+  | 'moderator:manage:guest_star'
+  | 'moderator:read:moderators'
+  | 'moderator:read:shield_mode'
+  | 'moderator:manage:shield_mode'
+  | 'moderator:read:shoutouts'
+  | 'moderator:manage:shoutouts'
+  | 'moderator:read:suspicious_users'
+  | 'moderator:read:unban_requests'
+  | 'moderator:manage:unban_requests'
+  | 'moderator:read:vips'
+  | 'moderator:read:warnings'
+  | 'moderator:manage:warnings'
     
-    // PubSub-specific Chat Scopes
-    | 'whispers:read'
+  // IRC Chat Scopes
+  | 'chat:edit'
+  | 'chat:read'
+  
+  // PubSub-specific Chat Scopes
+  | 'whispers:read'
+
+export type TwitchErrorResponse = {
+  error?: string
+  error_description?: string
+  state?: string
+  message?: string
+  status?: number
+}
+
+export type TwitchTokenResponse = {
+  access_token: string
+  expires_in: number
+  refresh_token: string
+  scope: Scopes[]
+  token_type: string
+}
+
+export interface TwitchUserResponse {
+  data: [{
+    id: string
+    login: string
+    display_name: string
+    type: string
+    broadcaster_type: string
+    description: string
+    profile_image_url: string
+    offline_image_url: string
+    view_count: number
+    email: string
+    created_at: string
+  }]
+}
+
+export type TwitchUser = TwitchUserResponse['data'][0]
+
+export interface TwitchRevokingResponse {
+  status: number
+  message?: string
+}  
