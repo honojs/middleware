d39f9b1dabd84c4e1e13aba9c411170a283531ea
Jonathan Haines <jonno.haines@gmail.com>
2025-03-03 16:12:41 +1100

    test(typia-validator): fix exclude directory

--- packages/typia-validator/vitest.config.ts
+++ packages/typia-validator/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig, defaultExclude } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    exclude: [...defaultExclude, 'tests'],
+    exclude: [...defaultExclude, 'test'],
   },
 })
