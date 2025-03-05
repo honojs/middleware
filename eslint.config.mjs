2842c291eb207e476db8169c7ea2e7b4aae3981e
Jonathan Haines <jonno.haines@gmail.com>
2025-03-05 13:01:54 +1100

    chore: cleanup ignored test-generated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import baseConfig from './packages/eslint-config/index.js'
 export default [
   ...baseConfig,
   {
-    ignores: ['**/dist/*', '**/test-generated/*'],
+    ignores: ['**/dist/*'],
   },
 ]
