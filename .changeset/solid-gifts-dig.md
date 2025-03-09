5cbd8a64c8a13d15e41bfb2dc1adc55db24a049f
wayofthepie <wayofthepie@gmail.com>
2025-03-09 08:52:29 +0000

    allow setting audience for oidc-auth

---
+++ .changeset/solid-gifts-dig.md
@@ -0,0 +1,5 @@
+---
+'@hono/oidc-auth': minor
+---
+
+Add support for setting audience in the OIDC_AUDIENCE environment variable
--- packages/oidc-auth/src/index.ts
+++ packages/oidc-auth/src/index.ts
@@ -58,6 +58,7 @@ type OidcAuthEnv = {
   OIDC_COOKIE_PATH?: string
   OIDC_COOKIE_NAME?: string
   OIDC_COOKIE_DOMAIN?: string
+  OIDC_AUDIENCE?: string
 }
 
 /**
@@ -307,6 +308,9 @@ const generateAuthorizationRequestUrl = async (
     // Google requires 'access_type=offline' and 'prompt=consent' to obtain a refresh token
     authorizationRequestUrl.searchParams.set('access_type', 'offline')
     authorizationRequestUrl.searchParams.set('prompt', 'consent')
+  }
+  if (env.OIDC_AUDIENCE) {
+    authorizationRequestUrl.searchParams.set('audience', env.OIDC_AUDIENCE)
   }
   return authorizationRequestUrl.toString()
 }
