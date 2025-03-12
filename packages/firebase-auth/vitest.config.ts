b4d1f2d5ea240d3b3559bad04592b91c561d8421
Jonathan Haines <jonno.haines@gmail.com>
2025-03-12 21:39:37 +1100

    ci: run workspace scripts

--- packages/firebase-auth/vitest.config.ts
+++ packages/firebase-auth/vitest.config.ts
@@ -1,7 +1,29 @@
+import type { Plugin } from 'vitest/config'
 import { defineProject } from 'vitest/config'
+
+const firebasePlugin = {
+  name: 'firebase',
+  async configureServer(server) {
+    const client = await import('firebase-tools')
+
+    void client.emulators.start({
+      cwd: server.config.root,
+      nonInteractive: true,
+      project: 'example-project12345',
+      projectDir: server.config.root,
+    })
+  },
+  async buildEnd() {
+    const controller = await import('firebase-tools/lib/emulator/controller')
+
+    await controller.cleanShutdown()
+  },
+} satisfies Plugin
 
 export default defineProject({
   test: {
     globals: true,
   },
+
+  plugins: [firebasePlugin],
 })
