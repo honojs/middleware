0d670bf2ee44b403061cad3c490cf5a4e77232f2
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-03-24 01:19:39 +0530

    chore(eslint-config): update dependencies and improve configuration

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,110 +1,106 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import tseslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import importX from 'eslint-plugin-import-x'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
+import nodePlugin from 'eslint-plugin-n'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default [
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:n/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      'import-x': importX,
-    },
+  js.configs.recommended,
+  nodePlugin.configs.recommended,
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+	{
+		plugins: {
+			'@typescript-eslint': tseslint,
+			'import-x': importX,
+		},
 
-    languageOptions: {
-      globals: {
-        fetch: false,
-        Response: false,
-        Request: false,
-        addEventListener: false,
-      },
+		languageOptions: {
+			globals: {
+				fetch: false,
+				Response: false,
+				Request: false,
+				addEventListener: false,
+			},
 
-      parser: tsParser,
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
+			parser: tsParser,
+			ecmaVersion: 2021,
+			sourceType: 'module',
+		},
 
-    rules: {
-      curly: ['error', 'all'],
-      quotes: ['error', 'single'],
-      semi: ['error', 'never'],
-      'no-debugger': ['error'],
+		rules: {
+			curly: ['error', 'all'],
+			quotes: ['error', 'single'],
+			semi: ['error', 'never'],
+			'no-debugger': ['error'],
 
-      'no-empty': [
-        'warn',
-        {
-          allowEmptyCatch: true,
-        },
-      ],
+			'no-empty': [
+				'warn',
+				{
+					allowEmptyCatch: true,
+				},
+			],
 
-      'no-process-exit': 'off',
-      'no-useless-escape': 'off',
+			'no-process-exit': 'off',
+			'no-useless-escape': 'off',
 
-      'prefer-const': [
-        'warn',
-        {
-          destructuring: 'all',
-        },
-      ],
+			'prefer-const': [
+				'warn',
+				{
+					destructuring: 'all',
+				},
+			],
 
-      'import-x/consistent-type-specifier-style': ['error', 'prefer-top-level'],
-      'import-x/order': [
-        'error',
-        {
-          groups: ['external', 'builtin', 'internal', 'parent', 'sibling', 'index'],
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-        },
-      ],
-      'import-x/no-duplicates': 'error',
+			'import-x/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+			'import-x/order': [
+				'error',
+				{
+					groups: [
+						'external',
+						'builtin',
+						'internal',
+						'parent',
+						'sibling',
+						'index',
+					],
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+				},
+			],
+			'import-x/no-duplicates': 'error',
 
-      'n/no-missing-import': 'off',
-      'n/no-missing-require': 'off',
-      'n/no-deprecated-api': 'off',
-      'n/no-unpublished-import': 'off',
-      'n/no-unpublished-require': 'off',
-      'n/no-unsupported-features/es-syntax': 'off',
-      'n/no-unsupported-features/node-builtins': 'off',
+			'n/no-missing-import': 'off',
+			'n/no-missing-require': 'off',
+			'n/no-deprecated-api': 'off',
+			'n/no-unpublished-import': 'off',
+			'n/no-unpublished-require': 'off',
+			'n/no-unsupported-features/es-syntax': 'off',
+			'n/no-unsupported-features/node-builtins': 'off',
 
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        {
-          prefer: 'type-imports',
-        },
-      ],
-      '@typescript-eslint/no-empty-object-type': 'off',
-      '@typescript-eslint/no-unsafe-function-type': 'off',
-      '@typescript-eslint/no-empty-function': [
-        'error',
-        {
-          allow: ['arrowFunctions'],
-        },
-      ],
-      '@typescript-eslint/no-unused-expressions': 'off',
-      '@typescript-eslint/no-empty-interface': 'off',
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-inferrable-types': 'off',
-      '@typescript-eslint/no-require-imports': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/no-var-requires': 'off',
-    },
-  },
+			'@typescript-eslint/consistent-type-imports': [
+				'error',
+				{
+					prefer: 'type-imports',
+				},
+			],
+			'@typescript-eslint/no-empty-object-type': 'off',
+			'@typescript-eslint/no-unsafe-function-type': 'off',
+			'@typescript-eslint/no-empty-function': [
+				'error',
+				{
+					allow: ['arrowFunctions'],
+				},
+			],
+			'@typescript-eslint/no-unused-expressions': 'off',
+			'@typescript-eslint/no-empty-interface': 'off',
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-inferrable-types': 'off',
+			'@typescript-eslint/no-require-imports': 'off',
+			'@typescript-eslint/no-unused-vars': 'warn',
+			'@typescript-eslint/no-var-requires': 'off',
+		},
+	},
 ]
