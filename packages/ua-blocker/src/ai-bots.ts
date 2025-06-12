ce69352352590fd14a9e57214d49834cf701e7fc
finxol <git@finxol.io>
2025-06-12 12:07:20 +0200

    fix: export `RegExp`s, not `string[]`s

--- packages/ua-blocker/src/ai-bots.ts
+++ packages/ua-blocker/src/ai-bots.ts
@@ -2,8 +2,8 @@ import { createMiddleware } from 'hono/factory'
 import { ROBOTS_TXT } from './generated'
 
 export {
-  ALL_BOTS as aiBots,
-  NON_RESPECTING_BOTS as nonRespectingAiBots,
+  ALL_BOTS_REGEX as aiBots,
+  NON_RESPECTING_BOTS_REGEX as nonRespectingAiBots,
   ROBOTS_TXT as AI_ROBOTS_TXT,
 } from './generated'
 
