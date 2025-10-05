2b5cd3193227a667d4b7ae0cdaa3c5fb2d1d4963
sinclair <haydn.developer@gmail.com>
2025-10-05 23:49:54 +0900

    (Coverage) Revert Comment UnpluginTypia Config to Resolve CI Error

--- packages/typia-validator/vitest.config.ts
+++ packages/typia-validator/vitest.config.ts
@@ -3,11 +3,9 @@ import { defineProject } from 'vitest/config'
 
 export default defineProject({
   plugins: [
-    // https://github.com/honojs/middleware/pull/1489#pullrequestreview-3301048779
-    //
-    // UnpluginTypia({
-    //   tsconfig: './tsconfig.build.json',
-    // }),
+    UnpluginTypia({
+      tsconfig: './tsconfig.build.json',
+    }),
   ],
   test: {
     globals: true,
