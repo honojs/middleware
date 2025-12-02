863f7c68e493d2c947cdf679c9f385175f205b15
Yusuke Wada <yusuke@kamawada.com>
2025-12-02 16:46:02 -0600

    remove examples

--- packages/capnweb/examples/client/client-batch.ts
+++
@@ -1,7 +0,0 @@
-import type { RpcStub } from 'capnweb';
-import { newHttpBatchRpcSession } from 'capnweb'
-import type { PublicApi } from '../src/my-api-server'
-
-const stub: RpcStub<PublicApi> = newHttpBatchRpcSession<PublicApi>('http://localhost:8787/api')
-
-console.log(await stub.hello("Cap'n Web"))
--- packages/capnweb/examples/client/client.ts
+++
@@ -1,7 +0,0 @@
-import type { RpcStub } from 'capnweb';
-import { newWebSocketRpcSession } from 'capnweb'
-import type { PublicApi } from '../src/my-api-server'
-
-using stub: RpcStub<PublicApi> = newWebSocketRpcSession<PublicApi>('ws://localhost:8787/api')
-
-console.log(await stub.hello("Cap'n Web"))
