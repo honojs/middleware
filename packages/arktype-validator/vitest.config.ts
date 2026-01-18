b41013eb6031ba0d317423a8e78256767af2162c
WolfieLeader <koren2318@gmail.com>
2026-01-19 01:12:04 +0200

    feat(vitest): add support for test-d.ts files and enable type checking

--- packages/arktype-validator/vitest.config.ts
+++ packages/arktype-validator/vitest.config.ts
@@ -3,6 +3,9 @@ import { defineProject } from 'vitest/config'
 export default defineProject({
   test: {
     globals: true,
-    include: ['src/**/*.test.ts'],
+    include: ['src/**/*.test.ts', 'src/**/*.test-d.ts'],
+    typecheck: {
+      enabled: true,
+    },
   },
 })
