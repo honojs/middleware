2ac1f6154ef42b6a02ab051a973c5b91d4a59ed6
Travis Zhang <wr.zhang25@gmail.com>
2026-03-02 17:10:37 +0800

    feat(swagger-ui): add support for custom CDN base URL in remoteAssets function

--- packages/swagger-ui/src/swagger/resource.ts
+++ packages/swagger-ui/src/swagger/resource.ts
@@ -3,12 +3,19 @@ export interface AssetURLs {
   js: string[]
 }
 
+const DEFAULT_CDN_BASE = 'https://cdn.jsdelivr.net/npm'
+
 type ResourceConfig = {
+  /** CDN base URL, e.g. https://unpkg.com for internal network mirror */
+  baseUrl?: string
   version?: string
 }
 
-export const remoteAssets = ({ version }: ResourceConfig): AssetURLs => {
-  const url = `https://cdn.jsdelivr.net/npm/swagger-ui-dist${
+export const remoteAssets = ({
+  baseUrl = DEFAULT_CDN_BASE,
+  version,
+}: ResourceConfig): AssetURLs => {
+  const url = `${baseUrl.replace(/\/$/, '')}/swagger-ui-dist${
     version !== undefined ? `@${version}` : ''
   }`
 
