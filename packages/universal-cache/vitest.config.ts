6d0ef1e2031dda747816fa38051569c00405fbab
raed bahri <raedbahri90@gmail.com>
2026-03-12 11:47:29 +0100

    feat(universal-cache): address review feedback

---
+++ packages/universal-cache/vitest.config.ts
@@ -0,0 +1,9 @@
+import { defineProject } from 'vitest/config'
+
+export default defineProject({
+  test: {
+    globals: true,
+    include: ['src/**/*.test.ts'],
+    exclude: ['src/**/*.workerd.test.ts'],
+  },
+})
